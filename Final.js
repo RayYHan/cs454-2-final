b99bda2d78b9bc81baebe4ce47e72020cb7d8e3b
Ray Y Han <giraiday@gmail.com>
2015-12-05 16:04:09 -0800

    4:03

--- Final.js
+++ Final.js
@@ -1,3 +1,21 @@
+/*
+The functions I am going to demo are _.defaults, _.pick and _.tap from the object category of the Underscore
+module.
+
+Instead of checking whether the value of a property is undefined before giving value,
+_.defaults fills in undefined properties in an object with default values.
+With the help of the _.each function, _.defaults can fill in undefined properties of all objects in a collection.
+
+In a function chain, sometime it is desirable to use the data in the middle of the chain without changing it.
+This can be done by returning the same data after working with it.
+But by using _.tap, developers can avoid the need of returning the original value.
+
+Sometimes it is necessary to construct an object with a subset of the properties from an existing object,
+_.pick can accomplish this with a single line when specified exist object and keys.
+The returned object will only contain properties with the specified keys.
+*/
+
+
 var _ = require('underscore')
 
 var students = [{Name: "John", Grade: "Senior", Gender: "Male"},
@@ -14,10 +32,6 @@ var freshmans = [{Name: "Jason", Gender: "Male"},
 var alumi = [{Name: "Joe", Gender: "Male"}]
 
 var newYear = function(){
-
-    displayLen = function(students){
-        console.log("number of Graduates: " + students.length)
-    };
 
     newGraduates = _.chain(students)
         .where({Grade: "Senior"})
@@ -43,11 +57,9 @@ var newYear = function(){
         .map(function(student){student.Grade = "Sophomore"; return student})
         .value();
 
-    assignFreshmanGrade = function(freshman){
-        _.defaults(freshman, {Grade: "Freshman"});
-    }
+    console.log("number of New Freshmans: " + freshmans.length)
 
-    _.each(freshmans, assignFreshmanGrade);
+    _.each(freshmans, function(freshman){_.defaults(freshman, {Grade: "Freshman"})});
 
     students = _.union(freshmans, newSophomores, newJuniors, newSeniors);
     alumi = _.union(alumi, newGraduates);
