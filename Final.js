ef1cb966e02ab749fd909dc2dc7fe13dfc7037d1
Ray Y Han <giraiday@gmail.com>
2015-12-05 18:55:08 -0800

    6:55

--- Final.js
+++ Final.js
@@ -1,21 +1,3 @@
-/*
-The functions I am going to demo are _.defaults and _.pick from the object category of the Underscore
-module.
-
-Instead of checking whether the value of a property is undefined before giving value,
-_.defaults fills in undefined properties in an object with default values.
-With the help of the _.each function, _.defaults can fill in undefined properties of all objects in a collection.
-
-In a function chain, sometime it is desirable to use the data in the middle of the chain without changing it.
-This can be done by returning the same data after working with it.
-But by using _.tap, developers can avoid the need of returning the original value.
-
-Sometimes it is necessary to construct an object with a subset of the properties from an existing object,
-_.pick can accomplish this with a single line when specified exist object and keys.
-The returned object will only contain properties with the specified keys.
-*/
-
-
 var _ = require('underscore');
 
 var students = [{Name: "John", Grade: "Senior", Gender: "Male"},
@@ -51,8 +33,6 @@ var newYear = function(){
         .where({Grade: "Freshman"})
         .map(function(student){student.Grade = "Sophomore"; return student})
         .value();
-
-    console.log("number of New Freshmen: " + freshmen.length)
 
     alumni = _.union(alumni, newGraduates);
     students = _.union(freshmen, newSophomores, newJuniors, newSeniors);
